b7a1bedcffb5551aee212521aaf2434147a79b0a
Rafael <rafael.acerqueirah@gmail.com>
2019-08-08 11:23:36 -0300

    Configure database and add Dev model

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
+require("dotenv/config");
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
 const routes = require("./routes");
+
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+  }@cluster0-vbayd.mongodb.net/omnistack?retryWrites=true&w=majority`,
+  { useNewUrlParser: true }
+);
 
 app.use(express.json());
 app.use(routes);
