53a3cf3c57396430e9b8659b58d557b42c720431
Rafael <rafael.acerqueirah@gmail.com>
2019-08-12 15:29:37 -0300

    Install and configure socket.io

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
 mongoose.connect(
   `mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PASSWORD
@@ -16,6 +19,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, (req, res) => {
+server.listen(3333, (req, res) => {
   console.log("BACKEND is RUNNING");
 });
