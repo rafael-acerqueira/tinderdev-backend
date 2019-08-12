1f60c968139ebbf7a696b981c748587b767bb1a8
Rafael <rafael.acerqueirah@gmail.com>
2019-08-12 15:58:50 -0300

    Create a object with relation between socket_id and user_id and pass through middleware to likecontroller

--- src/server.js
+++ src/server.js
@@ -8,12 +8,26 @@ const routes = require("./routes");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+const connectedDevs = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+  connectedDevs[user] = socket.id;
+});
+
 mongoose.connect(
   `mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PASSWORD
   }@cluster0-vbayd.mongodb.net/omnistack?retryWrites=true&w=majority`,
   { useNewUrlParser: true }
 );
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedDevs = connectedDevs;
+
+  return next();
+});
 
 app.use(cors());
 app.use(express.json());
